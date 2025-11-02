99a664a41bb9b543dc4e9bfbff00d0d46d5f2134
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-11-02 16:09:44 +0530

    new changes

---
+++ for-loop/ec2.tf
@@ -0,0 +1,47 @@
+resource "aws_instance" "terraform" {
+  for_each = toset(var.instances)
+  #count         = 4
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.terraform_security_group.id]
+
+  tags = {
+    Name = "${each.value}"
+    Value = "True"
+  }
+}
+
+resource "aws_security_group" "terraform_security_group"{
+    name = "allow_all_traffic"  # security group name
+    # description = ""
+    # vpc_id = ""
+
+    egress {
+      
+      from_port  = 0  # 0 means allow all ports alls ports
+      to_port    = 0
+      protocol   = "-1"  # all protocols
+      cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+    ingress {
+      
+      from_port  = 0  # 0 means allow all ports alls ports
+      to_port    = 0
+      protocol   = "-1"  # all protocols
+      cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+
+    tags = {  # name
+       
+       Name = "allow_all_tags"
+    
+    } 
+
+  # ingress = []
+  # egress = []
+
+}
---
+++ for-loop/variables.tf
@@ -0,0 +1,30 @@
+# case 1: convert list to map using toset([]) and use the list items as each.value instead of directly using as list by list.count.index
+
+#for_each = toset(var.instances)  --> each.value 
+
+# case 2: directly using map by each.key and each.value
+# variable "instances"{
+#  default = {
+#  {
+#   instance1 : "mongodb",
+#   instance2 : "redis"
+  
+#   } 
+  
+#   } 
+
+# for_each = var.instances ;   each.key --> instance1 ; each.value --> mongodb etc
+
+
+
+variable "instances"{
+    default = ["mongodb","redis","mysql","rabbitmq"]
+}
+
+variable "zone_id" {
+  default = "Z07155921YL2PZLNGBCDC"
+}
+
+variable "domain_name" {
+  default = "vinaymukkamalla.fun"
+}
